6a1284d02a640f62d9c5a2a33f80a71fa89303a0
nuridavid <nurieldavid@gmail.com>
2021-05-12 13:08:28 -0400

    basic user signup completed

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,14 +1,27 @@
-import React, { useRef } from "react";
-import { Form, Button, Card } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../AuthContext/AuthCont";
 function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
   const { signup } = useAuth();
-
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   async function handleSubmit(e) {
     e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError("Failed to create an account");
+    }
 
     signup(emailRef.current.value, passwordRef.current.value);
   }
@@ -17,7 +30,8 @@ function Signup() {
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
-          <Form>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
@@ -30,15 +44,13 @@ function Signup() {
               <Form.Label>Password Confirmation</Form.Label>
               <Form.Control type="password" ref={passwordConfirmRef} required />
             </Form.Group>
-            <Button onClick={handleSubmit} className="w-100">
+            <Button disabled={loading} className="w-100" type="submit">
               Sign Up
             </Button>
           </Form>
         </Card.Body>
       </Card>
-      <div className="w-100 text-center mt-2">
-        Already have an account? Log In
-      </div>
+      <div className="w-100 text-center mt-2">Already have an account?</div>
     </>
   );
 }
